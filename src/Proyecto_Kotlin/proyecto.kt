a474017be8ec49c784e87e7e5cfda8540224342b
fiorella195 <fiorella15082007@gmail.com>
2025-06-02 09:51:06 -0600

    proyecto son terminar

---
+++ src/Proyecto_Kotlin/proyecto.kt
@@ -0,0 +1,13 @@
+package Proyecto_Kotlin
+
+fun registraNombres (Nombre: String) {
+    val nombres = arrayOfNulls<String>(5)
+    nombres[0] = "Carlos"
+    nombres[1] = "Juan"
+    nombres[2] = "Ana"
+    nombres[3] = "Luis"
+    nombres[4] = "Sofia"
+
+}
+
+
